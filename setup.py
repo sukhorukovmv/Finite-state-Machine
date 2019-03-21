0bab2e00c05fd36465f048671e327a2b74db76d4
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-03-21 22:36:03 +0300

    setup.py update

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 
 setup(
-    name='finite-state-Machine',
+    name='finiteStateMachine',
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
-)
-
+    entry_point={
+        'console_scripts':
+            ['helloworld = src.core:print_message']
+        }
+    )
