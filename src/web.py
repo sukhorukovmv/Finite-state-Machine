a269d77c347824f8818400ada693c4769458edf9
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-03-24 23:09:30 +0300

    web.py added(begin logic for server)

---
+++ src/web.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+from src.core import get_message
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html',
+        message=get_message())
+
+def run_server():
+    app.run()
+
