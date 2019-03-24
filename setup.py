96e7530d1c3cefe78d611f9ff41812e7f8716915
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-03-24 22:08:15 +0300

    include version setings

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+import src
+
 setup(
     name='finiteStateMachine',
-    version='1.0',
+    version=src.__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     entry_points={
