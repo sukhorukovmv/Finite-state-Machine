999431808c053c5501a1d4cd9a0d4d68327ce142
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-03-25 23:50:44 +0300

    first unit tests added and manifest.in fixed

--- setup.py
+++ setup.py
@@ -4,20 +4,32 @@ from os.path import join, dirname
 import src
 
 setup(
-    name='finiteStateMachine',
+    name='finiteStateMachine', # name project for search in PyPI "pip install finiteStateMachine"
     include_package_data=True,
     version=src.__version__,
-    test_suite='./test/tests',
-    packages=find_packages(),
+    setup_requires=[], # all libraries on which the build depends
+    tests_require=[], # all libraries on which the run tests depends
+    packages=find_packages(), #(exclude=[".tests.*, .templates.*"]),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     entry_points={
         'console_scripts':[
             'helloworld = src.core:print_message', 
             'serve = src.web:run_server',
-            ]
-        },
+        ]
+    },
 
-    install_requires=[
-        'Flask==0.8'    
+    package_data={  # all files on which include in the final package
+    '': [
+        './test/*.py', 
+        'README.md', 
+        '*.png',
+        'MANIFEST.in',
+        ],
+    },
+
+    install_requires=[ # all libraries on which the package depends, except standart library
+        'Flask==0.9'    
     ]
-    )
+)
+#$ python -m unittest discover
+
---
+++ tests/test_tests.py
@@ -0,0 +1,28 @@
+import unittest
+from src.core import get_message
+
+class HelloworldTestCase(unittest.TestCase):
+    def test_helloworld(self):
+        self.assertEqual(get_message(), 'Hello World!')
+
+if __name__ == '__main__':
+    unittest.main()
+
+#import pytest
+
+#def setup_module(module):
+#    #init_something()
+#    pass
+#
+#def teardown_module(module):
+#    #teardown_something()
+#    pass
+#
+#def test_upper():
+#    assert 'foo'.upper() == 'FOO'
+#
+#def test_isupper():
+#    assert 'FOO'.isupper()
+#
+#def test_failed_upper():
+#    assert 'foo'.upper() == 'FOo'
