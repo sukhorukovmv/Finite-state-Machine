f7cc2dcc77e928326ee97d37ab0fe55bb2d98913
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-04-03 23:04:31 +0300

    add dashboard and authentification

--- src/app.py
+++ src/app.py
@@ -71,6 +71,52 @@ def register():
         return redirect(url_for('register'))
     return render_template('register.html', form=form)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        #create cursor
+        cur = mysql.connection.cursor()
+
+        #get user by username 
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            #get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            #Compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+            
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/dashboard') 
+def dashboard():
+    return render_template('dashboard.html');
+
 if __name__ == '__main__': 
     app.secret_key='secret123'
     app.run(debug=True)
