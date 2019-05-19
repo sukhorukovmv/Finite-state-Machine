9bbd2acccbcbe6bff0516561b0c8fb21aa3c66b4
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-05-19 13:09:50 +0300

    Parser of table created

--- app/static/js/generateTable.js
+++ app/static/js/generateTable.js
@@ -12,14 +12,41 @@ function createTable(size) {
     return table;
 }
 
+function step(num) {
+    var i = 0;
+    while (Math.pow(++i, 2) != num);
+    return i;
+}
+
 //считывает и строит граф, наверное стоит назвать create graph
 function readTable(table) {
+    var arr = [];
     return function() {
         table.each(function(row){
             $(this).find('td').each(function(cell){
-                console.log($(this).children().val());
+                arr.push($(this).children().val()); //происходит считывание таблицы в массив
             });
         });
+
+        step = Number(step(arr.length));
+        var dict = {};
+
+        //метод который должен вернуть json строку , после она должна передаваться на back
+        for (var i = step; i < arr.length; i = i + step){
+            dict[arr[i]] = null;
+            var d = {};
+            for (var j = 1; j < step; j++){
+                if (arr[i + j] != 0){
+                    d[arr[j]] = arr[i + j];
+                    dict[arr[i]] = d;
+                }   
+            }
+        }
+
+       // var stringDict = JSON.strigify(dict);
+        var obj = {"first_name" : "John", "last_name" : "Smith", "location" : "London"}
+        var s = JSON.stringify(dict)
+        console.log(s);
     };
 }
 
