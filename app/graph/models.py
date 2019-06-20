fdc6634d331295a4c42a08d6bf26587bcdb53607
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-06-21 00:37:32 +0300

    change draw

--- app/graph/models.py
+++ app/graph/models.py
@@ -1,8 +1,8 @@
-from flask import url_for
 from pygraphviz import *
 
 
 def createGraph(nodesDictionary):
+    print(nodesDictionary)
     Graph = AGraph(directed=True)
     for startNode in nodesDictionary:
         Graph.add_node(startNode)
@@ -17,4 +17,24 @@ def createGraph(nodesDictionary):
 
 
 def drawGraph(Graph):
-    Graph.draw('app/static/img/result.png', prog="twopi") # draw to png using circo
+    Graph.draw('app/static/img/result.png', prog="dot") # draw to png using circo
+
+def fulnessCheck(nodesDictionary):
+    '''
+    Принимает граф в виде словаря
+    Если граф не прошел проверку на полноту вернет False иначе вернет True
+    '''
+    for startNode in nodesDictionary:
+        if nodesDictionary.get(startNode) == None:
+            return False
+
+    return True 
+
+if __name__ == '__main__':
+    nodesDictionary = {'Home': {'Home': 'fine', 'Work': 'Go'}, 'Work': None}
+
+    nodesDictionary1 = {'Home': {'Home': 'fine', 'Work': 'Go'}, 'Work': {'Home': 'lai'}}
+
+    print(fulnessCheck(nodesDictionary))
+    print(fulnessCheck(nodesDictionary1))
+
