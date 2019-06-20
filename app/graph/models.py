58886d8970b069bd887cc5ebb6e0b7bff640b459
Sukhorukov Maksim <sukhorukov.maksim95@gmail.com>
2019-06-21 01:43:54 +0300

    add determination check

--- app/graph/models.py
+++ app/graph/models.py
@@ -1,4 +1,5 @@
 from pygraphviz import *
+from collections import Counter
 
 
 def createGraph(nodesDictionary):
@@ -23,6 +24,7 @@ def fulnessCheck(nodesDictionary):
     '''
     Принимает граф в виде словаря
     Если граф не прошел проверку на полноту вернет False иначе вернет True
+    Пример: {'Home': {'Home': 'fine', 'Work': 'Go'}, 'Work': None}
     '''
     for startNode in nodesDictionary:
         if nodesDictionary.get(startNode) == None:
@@ -30,11 +32,32 @@ def fulnessCheck(nodesDictionary):
 
     return True 
 
+def checkDeterm(nodesDictionary):
+    '''
+    Если граф не детерменированный возвращает False
+    {"Work":{"Home":"1","Basic":"1"},"Home":null,"Basic":null}
+    '''
+    for startNode in nodesDictionary:
+        arrayValue = []
+        endNodes = nodesDictionary.get(startNode)
+        if endNodes:
+            for endNode in endNodes:
+                arrayValue.append(endNodes.get(endNode))
+        duplicate = [arrayValue[i] for i in range(len(arrayValue)) if i == arrayValue.index(arrayValue[i])]
+        if len(duplicate) != 0:
+            return False
+    return True 
+
+
 if __name__ == '__main__':
     nodesDictionary = {'Home': {'Home': 'fine', 'Work': 'Go'}, 'Work': None}
 
     nodesDictionary1 = {'Home': {'Home': 'fine', 'Work': 'Go'}, 'Work': {'Home': 'lai'}}
+    
+    nodesDictionary2 = {"Work": {"Home": "1", "Basic": "1"}, "Home": None, "Basic": None}
 
-    print(fulnessCheck(nodesDictionary))
-    print(fulnessCheck(nodesDictionary1))
+    print(checkDeterm(nodesDictionary2))
+
+    #print(fulnessCheck(nodesDictionary))
+    #print(fulnessCheck(nodesDictionary1))
 
